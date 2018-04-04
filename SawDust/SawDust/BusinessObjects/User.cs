4e35eb6d47b1bbed0db1cb7824c9329797a28b0f
Praneeth Marella <praneethbabumarella@u.boisestate.edu>
2018-04-04 12:23:44 -0600

    fixed some build errors that arised from aaron's branch

--- SawDust/SawDust/BusinessObjects/User.cs
+++ SawDust/SawDust/BusinessObjects/User.cs
@@ -6,20 +6,6 @@ using System.Threading.Tasks;
 
 namespace SawDust.BusinessObjects
 {
-    class User
-    {
-        public string name { get; set; }
-        public string username { get; set; }
-        public string password { get; set; }
-
-        public User(string name, string username, string password)
-        {
-            this.name = name;
-            this.username = username;
-            this.password = password;
-        }
-
-    }
 
 
 }
