91bdb0e39c41b240f32fae3f7eafca4f462ffece
AaronRosenthal-boise <aaronrosenthal@u.boisestate.edu>
2018-04-03 23:26:35 -0600

    added dashboard+buttons, user class

---
+++ SawDust/SawDust/BusinessObjects/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SawDust.BusinessObjects
+{
+    class User
+    {
+        public string name { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+
+        public User(string name, string username, string password)
+        {
+            this.name = name;
+            this.username = username;
+            this.password = password;
+        }
+
+    }
+
+
+}
