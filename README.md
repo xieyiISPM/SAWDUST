6a22f8d03f95f080785ac9f469710836ed571588
Praneeth Marella <praneethbabumarella@u.boisestate.edu>
2018-04-04 13:37:43 -0600

    #35 #13 #8 Adding Sprint-1 Progress

---
+++ README.md
@@ -0,0 +1,28 @@
+## Team Name: IllegalNameException
+
+## Sponsor Name: Ashby's Cabinetry
+
+## Project Description:
+Build a windows application for Ashby's Cabinetry that lets them estimate costs for cabinet jobs
+
+## Team Members:
+
+Last Name       | First Name      | GitHub User Name     | Scrum Role
+--------------- | --------------- | -------------------- | ---------------
+Brewer          | Adam            | t3chmaSchool         | Developer
+Flores          | Gerardo         | gerflo123            | Developer
+Marella         | Praneeth        | pmarella2            | Product Owner
+Massaquoi       | Emmanuel        | Maxxquoi             | Developer
+Roberts         | Brody           | brodyrobertsBSU      | Developer
+Rosenthal       | Aaron           | AaronRosenthal-boise | Developer
+Wallace         | Robert          | robertwallace3       | Scrum Master
+Xie             | Yi              | xieyiISPM            | Developer
+
+## Team Velocity:
+
+Sprint | Estimated Velocity | Actual Velocity
+------ | ------------------ | ---------------
+1      | TBD                | TBD
+2      | TBD                | TBD
+3      | TBD                | TBD
+4      | TBD                | TBD
---
+++ SawDust/SawDust/ViewModel/ClientTabVM.cs
@@ -0,0 +1,94 @@
+﻿using SawDust.BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SawDust.ViewModel
+{
+    /* this is where the bindings are found for the ClientTab view
+     */
+    class ClientTabVM : ViewModelBase
+    {
+        #region Constructors
+        public ClientTabVM() {
+            Name = "Clients";
+        }
+        #endregion
+
+        #region Properties
+
+        private ObservableCollection<Client> _clients = new ObservableCollection<Client>();
+        public ObservableCollection<Client> Clients
+        {
+            get { return _clients; }
+        }
+        private Client _newClient;
+        public Client NewClient{
+            get
+            {
+                if (null == _newClient)
+                    _newClient = new Client();
+                return _newClient;
+            }
+            set {
+                _newClient = value;
+            }
+        }
+
+        public string NewClientName
+        {
+            get { return NewClient.ClientCompanyName; }
+            set { NewClient.ClientCompanyName = value; OnPropertyChanged("NewClientName"); }
+        }
+        
+        public string NewClientContact
+        {
+            get { return NewClient.ClientContactName; }
+            set { NewClient.ClientContactName = value; OnPropertyChanged("NewClientContact"); }
+        }
+        public string NewClientPhone
+        {
+            get { return NewClient.ClientContactPhone; }
+            set { NewClient.ClientContactPhone = value; OnPropertyChanged("NewClientPhone"); }
+        }
+        public string NewClientEmail
+        {
+            get { return NewClient.ClientContactEMail; }
+            set { NewClient.ClientContactEMail = value; OnPropertyChanged("NewClientEmail"); }
+        }
+        #endregion
+
+        RelayCommand _addClientCommand;
+        public ICommand AddClientCommand
+        {
+            get {
+                if (_addClientCommand == null)
+                {
+                    _addClientCommand = new RelayCommand(param => this.AddClient(param));
+                }
+                return _addClientCommand;
+            }
+            
+        }
+        public void AddClient(object param)
+        {
+            // validate stuff TBD
+
+            // save to database TBD
+            // add client to list            
+            _clients.Add(NewClient);
+            OnPropertyChanged("Clients");
+
+            // reset the view
+            NewClient = new Client();            
+            OnPropertyChanged("NewClientName");
+            OnPropertyChanged("NewClientContact");
+            OnPropertyChanged("NewClientPhone");
+            OnPropertyChanged("NewClientEmail");
+        }
+    }
+}
