a9c57f33b6ed59ef535e2201ff8567527dbb3e3e
Robert Wallace <robertwallace3@u.boisestate.edu>
2018-04-10 22:06:30 -0600

    started working on data access. added test project

---
+++ SawDust/SawDust/DataAccess/SqliteDataAccess.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using Microsoft.Data.Sqlite.Internal;
+
+namespace SawDust.DataAccess
+{
+  
+    public class SqliteDataAccess
+    {
+        public string databaseFileName = "data.db";
+        public string dbFile;
+        public SqliteDataAccess()
+        {
+            dbFile = AppGlobals.gLocalFolder.Path + Path.DirectorySeparatorChar + databaseFileName;
+            if (!File.Exists(dbFile))
+            {
+                createDbFile();
+            }   
+        }
+        private void createDbFile()
+        {
+            SqliteEngine.UseWinSqlite3(); //Configuring library to use SDK version of SQLite
+            using (SqliteConnection db = new SqliteConnection("Filename="+dbFile))
+            {
+                db.Open();
+                String tableCommand = "CREATE TABLE IF NOT EXISTS CLIENTS (ClientId	INTEGER PRIMARY KEY AUTOINCREMENT,	CompanyName	TEXT, " +
+                    "ContactName	TEXT,	PhoneNumber	TEXT,	Email	TEXT,	Status	TEXT,	InsertEtime	INTEGER,	LastUpdatedEtime	INTEGER ) ";
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+                try
+                {
+                    createTable.ExecuteReader();
+                }
+                catch (SqliteException e)
+                {
+                    //Do nothing
+                }
+            }
+        }
+    }
+}
---
+++ SawDust/UnitTestSawDust/UnitTest.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SawDust.DataAccess;
+
+namespace UnitTestSawDust
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSqliteDaConstructor()
+        {
+            SqliteDataAccess da = new SqliteDataAccess();
+            Assert.IsFalse(String.IsNullOrEmpty(da.dbFile));
+            Assert.IsTrue(File.Exists(da.dbFile));
+        }
+    }
+}
