05657235400c25a054e3fd76c5dba64b7bba6ff0
AaronRosenthal-boise <aaronrosenthal@u.boisestate.edu>
2018-04-27 11:43:48 -0600

    added job tab, subtabs

---
+++ SawDust/SawDust/Views/Subviews/JobsTab/Rooms.xaml.cs
@@ -0,0 +1,33 @@
+﻿using SawDust.ViewModel.SubviewModel.Jobs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SawDust.Views.Subviews.JobsTab
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Rooms : Page
+    {
+        public Rooms()
+        {
+            this.InitializeComponent();
+            this.DataContext = new RoomsVM();
+        }
+    }
+}
